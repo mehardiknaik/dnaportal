7572cd55a59514da5dcd8170600996714de35404
Hardik Naik <hardiknaik7444@gmail.com>
2022-03-20 12:28:44 +0530

    Update style.js

--- src/components/Status/style.js
+++ src/components/Status/style.js
@@ -1,4 +1,6 @@
 import styled from "styled-components";
+import { css } from "styled-components";
+import { keyframes } from "styled-components";
 
 export const StatusWrapper = styled.div`
   display: flex;
@@ -11,14 +13,26 @@ export const StatusWrapper = styled.div`
     gap: 1rem;
   }
 `;
+const ripple = keyframes`
+0% {
+  opacity: 1;
+  transform: scale(.8);
+}
+
+100% {
+  opacity: 0;
+  transform: scale(1.4);
+}
+`;
 export const Circle = styled.div`
   height: 50px;
   width: 50px;
-  background: ${({Status}) => (Status === "Active" ? "#05c000" : "#ff0000")};
+  background: ${({ Status }) => (Status === "Active" ? "#05c000" : "#ff0000")};
   border-radius: 50%;
   align-items: center;
   display: flex;
   justify-content: center;
+  animation: ${({ Status }) => (Status === "Active" ? `` : css`${ripple} 1.2s infinite ease-in-out`)};
   & >img {
     transform: ${({Status}) => (Status === "Active" ?'scale(1)':'scaleY(-1)')};
   }
@@ -26,9 +40,10 @@ export const Circle = styled.div`
 export const Circle2 = styled.div`
   height: 50px;
   width: 50px;
-  background: ${({Status}) => (Status === "Online" ? "#05c000" : "#f9aa00")};
+  background: ${({ Status }) => (Status === "Online" ? "#05c000" : "#f9aa00")};
   border-radius: 50%;
   align-items: center;
   display: flex;
   justify-content: center;
+  animation: ${({ Status }) => (Status === "Online" ? `` : css`${ripple} 1.2s infinite ease-in-out`)};
 `;
